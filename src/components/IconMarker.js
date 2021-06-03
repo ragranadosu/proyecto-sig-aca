08e974c8f05f12e3eb3466486eb9443086d54895
Raúl Granados <raul.granados12@gmail.com>
2021-06-02 19:00:48 -0600

    Agregando icono de marcador

---
+++ src/components/IconMarker.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import IconSvg from '../assets/venue_location_icon.svg';
+import L from 'leaflet';
+
+const IconMarker = L.icon({
+    iconUrl: IconSvg,
+    iconRetinaUrl: IconSvg,
+    iconAnchor: null,
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: [35, 35],
+    className: "Leaflet-vanue-icon"
+})
+
+export default IconMarker
--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import Icon from './IconMarker';
 import 'leaflet/dist/leaflet.css'
 
 const MapView = () => {
     return (
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <MapContainer center={[13.6527, -88.8684]} zoom={9} scrollWheelZoom={false}>
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[51.505, -0.09]}>
+            <Marker position={[13.6527, -88.8684]} icon={ Icon }>
                 <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
                 </Popup>
