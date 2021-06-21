bc47912e204806ace37506a1434e0bc6af61abb2
ragranados <raul.granados12@gmail.com>
2021-06-21 17:35:34 -0600

    Agregando logica para seleccionar ruta

--- src/components/MapView/index.js
+++ src/components/MapView/index.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {MapContainer, TileLayer, Marker, Popup, GeoJSON, useMap, useMapEvent} from 'react-leaflet'
+import {MapContainer, TileLayer, Marker, Popup, GeoJSON, ZoomControl, Pane, useMap, useMapEvent} from 'react-leaflet'
 import color from 'randomcolor';
 import Icon from '../IconMarker';
 import 'leaflet/dist/leaflet.css'
@@ -12,19 +12,23 @@ const MapUtil = () => {
     return null;
 }
 
-const MapEvents = () => {
-    const map = useMapEvent({
-        click: () => {
-            console.log(map.getCenter());
-        }
-    })
-    return null;
-}
 
 const MapView = (props) => {
     const [routes, setRoutes] = useState([]);
     const [center, setCenter] = useState([13.7153719325982, -89.19499397277833]);
     const [loading, isLoading] = useState(true);
+    const [selectedRoute, setSelectedRoute] = useState(null);
+    const colors = [];
+
+    const MapEvents = () => {
+        const map = useMapEvent({
+            click: () => {
+                console.log(map.getCenter());
+                setSelectedRoute(null);
+            }
+        })
+        return null;
+    }
 
     useEffect(() => {
         const routes = props.rutas || Routes.default;
@@ -44,23 +48,36 @@ const MapView = (props) => {
             {!loading ?
                 <MapContainer center={props.center || (center)} zoom={13} scrollWheelZoom={false}>
                     {/*<MapUtil/>*/}
-                    {/*<MapEvents/>*/}
+                    {<MapEvents/>}
+                    <ZoomControl position="bottomleft"/>
                     <TileLayer
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
 
-                    {routes.map((route) => {
+                    {routes.map((route, index) => {
+                        console.log(index == selectedRoute);
                         return (
                             <GeoJSON
+                                eventHandlers={{
+                                    click: () => {
+                                        setSelectedRoute(index);
+                                    },
+                                }}
                                 key={route.name}
                                 data={route}
-                                style={{
-                                    color: color({
-                                    luminosity: 'dark'
-                                })}}
-                                attribution="&copy; credits due..."
-                            />
+                                pathOptions={{
+                                    color: selectedRoute == index ? 'black' : color({
+                                        luminosity: 'dark'
+                                    }),
+                                    opacity: (selectedRoute != null && selectedRoute != index) ? 0.1 : 1
+                                }}
+                                //attribution="&copy; credits due..."
+                            >
+                                <Popup>
+                                    {route.name}
+                                </Popup>
+                            </GeoJSON>
                         )
                     })}
 
