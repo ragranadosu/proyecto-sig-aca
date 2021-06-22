19de54ed22dedc16bc2a3a0166519dde15f02969
ragranados <raul.granados12@gmail.com>
2021-06-22 07:39:24 -0600

    Cambiando parametros de componente

--- src/components/MapView/index.js
+++ src/components/MapView/index.js
@@ -1,24 +1,32 @@
 import React, {useEffect, useState} from "react";
-import {MapContainer, TileLayer, Marker, Popup, GeoJSON, ZoomControl, Pane, useMap, useMapEvent} from 'react-leaflet'
+import {MapContainer, TileLayer, Marker, Popup, GeoJSON, ZoomControl, useMap, useMapEvent} from 'react-leaflet'
 import color from 'randomcolor';
 import Icon from '../IconMarker';
 import 'leaflet/dist/leaflet.css'
 import Routes from '../../data/RoutesNames';
 import RoutesService from '../../services/busesRoutesService';
 
-const MapUtil = () => {
-    const map = useMap();
-    console.log(map.getCenter());
-    return null;
-}
+/**
+ *
+ * @param rutas
+ * @param centerProp
+ * @returns {JSX.Element}
+ * @constructor
+ */
 
-
-const MapView = (props) => {
+const MapView = ({rutas, centerProp}) => {
     const [routes, setRoutes] = useState([]);
     const [center, setCenter] = useState([13.7153719325982, -89.19499397277833]);
     const [loading, isLoading] = useState(true);
     const [selectedRoute, setSelectedRoute] = useState(null);
     const [colors, setColors] = useState([]);
+
+    const MapUtil = () => {
+        const map = useMap();
+        console.log(map.getCenter());
+
+        return null;
+    }
 
     const MapEvents = () => {
         const map = useMapEvent({
@@ -27,19 +35,19 @@ const MapView = (props) => {
                 setSelectedRoute(null);
             }
         })
+
         return null;
     }
 
     useEffect(() => {
-        const routes = props.rutas || Routes.default;
+        const routes = rutas || Routes.default;
         const colorsArray = [];
+
         RoutesService.getRoutes(routes).then((response) => {
 
             for (var i = 0; i < response.length; i++) {
                 colorsArray.push(
-                    color({
-                        luminosity: 'dark'
-                    })
+                    color({luminosity: 'dark'})
                 );
             }
 
@@ -56,7 +64,7 @@ const MapView = (props) => {
     return (
         <div>
             {!loading ?
-                <MapContainer center={props.center || (center)} zoom={13} scrollWheelZoom={false}>
+                <MapContainer center={centerProp || (center)} zoom={13} scrollWheelZoom={false}>
                     {/*<MapUtil/>*/}
                     {<MapEvents/>}
                     <ZoomControl position="bottomleft"/>
@@ -78,7 +86,7 @@ const MapView = (props) => {
                                 data={route}
                                 pathOptions={{
                                     color: selectedRoute == route.name ? 'black' : colors[index],
-                                    opacity: (selectedRoute != null && selectedRoute != route.name) ? 0.1 : 1
+                                    opacity: (selectedRoute != null && selectedRoute != route.name) ? 0.2 : 1
                                 }}
                                 //attribution="&copy; credits due..."
                             >
